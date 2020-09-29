ecff955878925e7b9b7d4199c2ca798cf81e866e
JosCla <jozerclaver@gmail.com>
2020-09-28 20:44:58 -0600

    Added player class (unfinished).

---
+++ src/player.cpp
@@ -0,0 +1,33 @@
+// player.cpp
+
+// libraries
+#include <glm/gtc/type_ptr.hpp> // type pointer
+
+// stdlib
+#include <iostream> // for cout
+
+// our files
+#include <camera.h> // for Camera class
+#include <shapes.h> // for Ellipsoid class
+
+class Player
+{
+	// constructor
+	// get/set for all attributes
+	// pass frame function
+	// render player?
+	public:
+		Player(glm::vec3, glm::vec3);
+
+	// position, direction, velocity
+	// camera
+	// hitbox
+	// model?
+	private:
+		glm::vec3 Position;
+		glm::vec3 Direction;
+		glm::vec3 Velocity;
+
+		Camera PlayerCamera;
+		Ellipsoid PlayerHitbox;
+};
